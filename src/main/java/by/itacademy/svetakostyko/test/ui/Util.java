f608c0bd50c3f11050a30f835d6f576b9a85e64f
Sveta <skostyko@gmail.com>
2023-03-27 11:44:39 +0200

    refactor Util class, add waitForElementToBeVisibleByXPath

--- src/main/java/by/itacademy/svetakostyko/test/ui/Util.java
+++ src/main/java/by/itacademy/svetakostyko/test/ui/Util.java
@@ -1,4 +1,14 @@
 package by.itacademy.svetakostyko.test.ui;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Util {
+    public static void waitForElementToBeVisibleByXPath(WebDriver driver, String xpath, int seconds) {
+            new WebDriverWait(driver, Duration.ofSeconds(seconds))
+                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        }
 }
