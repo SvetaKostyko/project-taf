fe854e5f37d0184191841657f5b3ac190c0ed3fb
Sveta <skostyko@gmail.com>
2023-03-27 19:39:12 +0200

    refactor KeysPage

--- src/main/java/by/itacademy/svetakostyko/test/ui/KeysPage.java
+++ src/main/java/by/itacademy/svetakostyko/test/ui/KeysPage.java
@@ -1,15 +1,8 @@
 package by.itacademy.svetakostyko.test.ui;
 
 public class KeysPage {
-    public static String EMAIL = "123tfh123@mail.ru";
-    public static String PASSWORD = "bh123bh";
-    public static String ATTRIBUTE_OF_USER = "onclick";
-    public static String USER_NAME = "Василий";
-    public static String LABEL_OF_LOGIN = "return profile_menu();";
-    public static String LABEL_OF_LOGOUT = "return show_object('auth');";
-    public static String TEXT_OF_ERROR_MASSAGE = "Ошибка! Не правильно введен E-mail или Пароль.";
+       public static String TEXT_OF_ERROR_MASSAGE = "Ошибка! Не правильно введен E-mail или Пароль.";
     public static String PRODUCT = "Костюмы";
-
 
 
 }
