0c4ce1e8e784d7ea9c37a9d6da431fa001c2bfc8
Sveta <skostyko@gmail.com>
2023-03-27 10:57:25 +0200

    refactor KeysPage, add LABEL_OF_LOGOUT

--- src/main/java/by/itacademy/svetakostyko/test/ui/KeysPage.java
+++ src/main/java/by/itacademy/svetakostyko/test/ui/KeysPage.java
@@ -6,6 +6,7 @@ public class KeysPage {
     public static String ATTRIBUTE_OF_USER = "onclick";
     public static String USER_NAME = "Василий";
     public static String LABEL_OF_LOGIN = "return profile_menu();";
+    public static String LABEL_OF_LOGOUT = "return show_object('auth');";
     public static String TEXT_OF_ERROR_MASSAGE = "Ошибка! Не правильно введен E-mail или Пароль.";
     public static String PRODUCT = "Костюмы";
 
