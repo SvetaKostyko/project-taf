b2e25af2cae5f8c0db2248f58a0a905124a5d5b1
Sveta <skostyko@gmail.com>
2023-03-27 19:40:57 +0200

    refactor UserPage

--- src/main/java/by/itacademy/svetakostyko/test/ui/UserPage.java
+++ src/main/java/by/itacademy/svetakostyko/test/ui/UserPage.java
@@ -1,4 +1,10 @@
 package by.itacademy.svetakostyko.test.ui;
 
 public class UserPage {
+    public static String EMAIL = "123tfh123@mail.ru";
+    public static String PASSWORD = "bh123bh";
+    public static String USER_NAME = "Василий";
+    public static String ATTRIBUTE_OF_USER = "onclick";
+    public static String LABEL_OF_LOGIN = "return profile_menu();";
+    public static String LABEL_OF_LOGOUT = "return show_object('auth');";
 }
