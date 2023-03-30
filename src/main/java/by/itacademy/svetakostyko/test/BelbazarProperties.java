8f8ce71b21cac1793ea626eca131e09196c8bee5
Sveta <skostyko@gmail.com>
2023-03-30 23:39:32 +0200

    delete BelbazarProperties.java

--- src/main/java/by/itacademy/svetakostyko/test/BelbazarProperties.java
+++
@@ -1,7 +0,0 @@
-package by.itacademy.svetakostyko.test;
-
-public class BelbazarProperties {
-    public static String EMAIL = "123tfh123@mail.ru";
-    public static String PASSWORD = "bh123bh";
-    public static String USER_NAME = "Василий";
-}
