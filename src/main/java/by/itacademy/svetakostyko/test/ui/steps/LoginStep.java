4081764ea0c582bb868d1459a51e0ffd6da3178f
Sveta <skostyko@gmail.com>
2023-03-30 09:49:47 +0200

    refactor LoginStep, add stepLoginUserWithPasswordOnly

--- src/main/java/by/itacademy/svetakostyko/test/ui/steps/LoginStep.java
+++ src/main/java/by/itacademy/svetakostyko/test/ui/steps/LoginStep.java
@@ -4,7 +4,7 @@ import by.itacademy.svetakostyko.test.ui.pages.LoginPage;
 
 public class LoginStep {
 
-    public static void loginUser(String email, String password) {
+    public static void StepLoginUser(String email, String password) {
         LoginPage loginPage = new LoginPage();
         loginPage.openLogin();
         loginPage.inputEmail(email);
@@ -13,10 +13,17 @@ public class LoginStep {
 
     }
 
-    public static void loginUserWithEmailOnly(String email) {
+    public static void stepLoginUserWithEmailOnly(String email) {
         LoginPage loginPage = new LoginPage();
         loginPage.openLogin();
         loginPage.inputEmail(email);
+        loginPage.clickLogin();
+    }
+
+    public static void stepLoginUserWithPasswordOnly(String password) {
+        LoginPage loginPage = new LoginPage();
+        loginPage.openLogin();
+        loginPage.inputPassword(password);
         loginPage.clickLogin();
     }
 }
