a8d6b31145360abe3d0aca27623560df2c857b88
Sveta <skostyko@gmail.com>
2023-03-30 23:46:30 +0200

    refactor BelbazarPage

--- src/main/java/by/itacademy/svetakostyko/test/ui/BelbazarPage.java
+++ src/main/java/by/itacademy/svetakostyko/test/ui/BelbazarPage.java
@@ -3,12 +3,6 @@ package by.itacademy.svetakostyko.test.ui;
 public class BelbazarPage {
     public static String URL = "https://belbazar24.by/";
     public static String BUTTON_OF_PROFILE = "//a[contains(@class,'top_block_link profile')]";
-    public static String EMAIL_FIELD = "//input[@name='info[login]']";
-    public static String PASSWORD_FIELD = "//div[@class='auth_mode email']//input[@type='password']";
-    public static String LOGIN_BUTTON = "//div[@class='button blue'][contains(text(),'Войти')]";
-    public static String LABEL_OF_USER = "//a[@href='/profile/']";
-    public static String STATUS_OF_LOGIN = "//a[contains(@class,'top_block_link profile')]";
-    public static String ERROR_MASSAGE = "//div[@class='auth_error_message'][contains(text(),'Ошибка!')]";
     public static String SEARCH_FIELD = "//input[@placeholder='Поиск товара']";
     public static String SEARCH_BUTTON = "//div[@class='top_search_button']";
     public static String BRAND_NAME = "(//div[@class='product_item_dop'])[1]";
@@ -20,14 +14,7 @@ public class BelbazarPage {
     public static String BRAND_IN_BASKET = "//div[@class='basket_item_brand']";
     public static String CODE_IN_BASKET = "(//div[@class='param']//div[@class='rightText'])[2]";
     public static String SIZE_OF_PRODUCT_IN_BASKET = "//div[@class='basket_size_box']";
-    public static String BUTTON_OF_LOGOUT = "//div[@class='l_menu_3ur_item']//a[@href='/profile/exit/']";
-    public static String ATTRIBUTE_OF_USER = "onclick";
-    public static String LABEL_OF_LOGIN = "return profile_menu();";
-    public static String LABEL_OF_LOGOUT = "return show_object('auth');";
 
-    public static String EMAIL = "123tfh123@mail.ru";
-    public static String PASSWORD = "bh123bh";
-    public static String USER_NAME = "Василий";
 }
 
 
