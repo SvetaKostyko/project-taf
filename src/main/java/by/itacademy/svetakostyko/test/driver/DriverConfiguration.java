1b529e1b999b52ae3736121f3e51ed7deeb29ff6
Sveta <skostyko@gmail.com>
2023-03-30 07:49:18 +0200

    add DriverConfiguration

---
+++ src/main/java/by/itacademy/svetakostyko/test/driver/DriverConfiguration.java
@@ -0,0 +1,23 @@
+package by.itacademy.svetakostyko.test.driver;
+
+import by.itacademy.svetakostyko.test.ui.BelbazarPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverConfiguration {
+    private static WebDriver driver;
+
+    static {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("start-maximized");
+        driver = new ChromeDriver(chromeOptions);
+//        driver.navigate().to(BelbazarPage.URL);
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}
