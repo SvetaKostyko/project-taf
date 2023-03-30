a649c50c839c53e477cd1f7a088a92fbbec8e793
Sveta <skostyko@gmail.com>
2023-03-30 23:32:45 +0200

    refactor BelbazarTest

--- src/test/java/by/itacademy/svetakostyko/test/BelbazarTest.java
+++ src/test/java/by/itacademy/svetakostyko/test/BelbazarTest.java
@@ -10,6 +10,7 @@ import by.itacademy.svetakostyko.test.ui.steps.LoginStep;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import static by.itacademy.svetakostyko.test.BelbazarProperties.*;
 
@@ -24,7 +25,7 @@ public class BelbazarTest {
     @Test
     @DisplayName("Авторизация с корректными данными")
     public void testLoginUserWithValidData() {
-        LoginStep.StepLoginUser(EMAIL, PASSWORD);
+        LoginStep.stepLoginUser(EMAIL, PASSWORD);
         Assertions.assertEquals(USER_NAME, LoginPage.getUserName());
     }
 
@@ -54,14 +55,17 @@ public class BelbazarTest {
     public void testSearchCostume() {
         driver.findElement(By.xpath(BelbazarPage.SEARCH_FIELD)).sendKeys(TextPage.PRODUCT);
         driver.findElement(By.xpath(BelbazarPage.SEARCH_BUTTON)).click();
-        String firstProductOnPageBrand = Util.waitForElementToBeVisibleByXPath(driver, BelbazarPage.BRAND_NAME, 3);
+        String firstProductOnPageBrand = Util
+                .waitForElementToBeVisibleByXPath(driver, BelbazarPage.BRAND_NAME, 3).getText();
         String firstProductOnPageCode = driver.findElement(By.xpath((BelbazarPage.CODE_OF_PRODUCT))).getText().substring(5, 9);
         driver.findElement(By.xpath(BelbazarPage.BASKET_BUTTON)).click();
-        String sizeOfProduct = Util.waitForElementToBeVisibleByXPath(driver, BelbazarPage.SIZE_BUTTON, 3);
+        String sizeOfProduct = Util
+                .waitForElementToBeVisibleByXPath(driver, BelbazarPage.SIZE_BUTTON, 3).getText();
         driver.findElement(By.xpath(BelbazarPage.SIZE_BUTTON)).click();
         driver.findElement(By.xpath(BelbazarPage.BUTTON_TO_BASKET)).click();
         driver.findElement(By.xpath(BelbazarPage.TOP_BASKET)).click();
-        String productInBasket = Util.waitForElementToBeVisibleByXPath(driver, BelbazarPage.BRAND_IN_BASKET, 3);
+        String productInBasket = Util
+                .waitForElementToBeVisibleByXPath(driver, BelbazarPage.BRAND_IN_BASKET, 3).getText();
         String codeOfProductInBasket = driver.findElement(By.xpath(BelbazarPage.CODE_IN_BASKET)).getText();
         String sizeInBasket = driver.findElement(By.xpath(BelbazarPage.SIZE_OF_PRODUCT_IN_BASKET)).getText();
         Assertions.assertEquals(firstProductOnPageBrand, productInBasket);
@@ -72,17 +76,8 @@ public class BelbazarTest {
     @Test
     @DisplayName("Выход из учетной записи")
     public void testLogOut() {
-        driver.findElement(By.xpath(BelbazarPage.BUTTON_OF_PROFILE)).click();
-        driver.findElement(By.xpath(BelbazarPage.EMAIL_FIELD))
-                .sendKeys(UserPage.EMAIL);
-        driver.findElement(By.xpath(BelbazarPage.PASSWORD_FIELD))
-                .sendKeys(UserPage.PASSWORD);
-        driver.findElement(By.xpath(BelbazarPage.LOGIN_BUTTON)).click();
-        driver.findElement(By.xpath(BelbazarPage.LABEL_OF_USER)).click();
-        driver.findElement(By.xpath(BelbazarPage.BUTTON_OF_LOGOUT)).click();
-        String actualLabelOfLogIn = driver.findElement(By.xpath(BelbazarPage.STATUS_OF_LOGIN))
-                .getAttribute(UserPage.ATTRIBUTE_OF_USER);
-        Assertions.assertEquals(UserPage.LABEL_OF_LOGOUT, actualLabelOfLogIn);
+        LoginStep.stepLogout(EMAIL, PASSWORD);
+        Assertions.assertTrue(LoginPage.isUserEmpty());
     }
 
     @AfterEach
