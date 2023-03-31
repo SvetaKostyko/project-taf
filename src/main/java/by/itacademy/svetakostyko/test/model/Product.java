56dba1c783b25e0f9d31b0af793d0136cf7bb633
Sveta <skostyko@gmail.com>
2023-03-31 10:26:03 +0200

    add Product

---
+++ src/main/java/by/itacademy/svetakostyko/test/model/Product.java
@@ -0,0 +1,9 @@
+package by.itacademy.svetakostyko.test.model;
+
+public class Product {
+    String firstProductOnPageBrand;
+    String firstProductOnPageCode;
+    String firstProductOnPagePrice;
+    String sizeOfProduct;
+
+}
