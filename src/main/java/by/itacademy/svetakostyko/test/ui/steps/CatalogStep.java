4448631e1dc177a237f424adab722d999ba82d07
Sveta <skostyko@gmail.com>
2023-03-31 10:29:42 +0200

    Add CatalogStep

---
+++ src/main/java/by/itacademy/svetakostyko/test/ui/steps/CatalogStep.java
@@ -0,0 +1,17 @@
+package by.itacademy.svetakostyko.test.ui.steps;
+
+import by.itacademy.svetakostyko.test.model.Product;
+import by.itacademy.svetakostyko.test.ui.pages.CatalogPage;
+
+public class CatalogStep {
+
+    public static Product putProductIntoBasket(String productCategory) {
+        CatalogPage catalogPage = new CatalogPage();
+        catalogPage.searchForProducts(productCategory);
+        Product product = catalogPage.putFirstProductIntoBasket();
+        return product;
+    }
+
+    public static Product getProductFromBasket() {
+    }
+}
