529f8bde083e02ba022edc9e9191b87cc96fa132
Sveta <skostyko@gmail.com>
2023-03-31 10:51:22 +0200

    refactor Product

--- src/main/java/by/itacademy/svetakostyko/test/model/Product.java
+++ src/main/java/by/itacademy/svetakostyko/test/model/Product.java
@@ -1,9 +1,13 @@
 package by.itacademy.svetakostyko.test.model;
 
 public class Product {
-    String firstProductOnPageBrand;
-    String firstProductOnPageCode;
-    String firstProductOnPagePrice;
-    String sizeOfProduct;
+    public static String firstProductOnPageBrand;
+    public static String firstProductOnPageCode;
+    public static String firstProductOnPagePrice;
+    public static String sizeOfProduct;
+    public static String productInBasket;
+    public static String codeOfProductInBasket;
+    public static String sizeInBasket;
 
+    public static String priceInBasket;
 }
