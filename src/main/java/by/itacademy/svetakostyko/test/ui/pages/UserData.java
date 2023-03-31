727dd19c6d2cd5c4bd19c118848fb94caf161f91
Sveta <skostyko@gmail.com>
2023-03-31 20:32:58 +0200

    refactor UserData

--- src/main/java/by/itacademy/svetakostyko/test/ui/pages/UserData.java
+++ src/main/java/by/itacademy/svetakostyko/test/ui/pages/UserData.java
@@ -1,7 +1,7 @@
 package by.itacademy.svetakostyko.test.ui.pages;
 
 public class UserData {
-    public static String EMAIL = "123tfh123@mail.ru";
+    public static final String EMAIL = "123tfh123@mail.ru";
     public static String PASSWORD = "bh123bh";
     public static String USER_NAME = "Василий";
    }
