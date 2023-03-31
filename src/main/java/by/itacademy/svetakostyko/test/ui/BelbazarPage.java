b0826fffc6aef8640d79f8df4aacedcb01267dc7
Sveta <skostyko@gmail.com>
2023-03-31 14:05:11 +0200

    refactor BelbazarPage

--- src/main/java/by/itacademy/svetakostyko/test/ui/BelbazarPage.java
+++ src/main/java/by/itacademy/svetakostyko/test/ui/BelbazarPage.java
@@ -2,9 +2,6 @@ package by.itacademy.svetakostyko.test.ui;
 
 public class BelbazarPage {
     public static String URL = "https://belbazar24.by/";
-    //public static String BUTTON_OF_PROFILE = "//a[contains(@class,'top_block_link profile')]";
-
-
 }
 
 
