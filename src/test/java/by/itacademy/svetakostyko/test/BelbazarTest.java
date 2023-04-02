2031d727a43efbb86cf009e20b1e2e99d106b770
Sveta <skostyko@gmail.com>
2023-04-02 14:31:09 +0200

    refactor BelbazarTest

--- src/test/java/by/itacademy/svetakostyko/test/BelbazarTest.java
+++ src/test/java/by/itacademy/svetakostyko/test/BelbazarTest.java
@@ -4,68 +4,74 @@ import by.itacademy.svetakostyko.test.driver.DriverConfiguration;
 import by.itacademy.svetakostyko.test.model.Product;
 import by.itacademy.svetakostyko.test.ui.pages.BelbazarPage;
 import by.itacademy.svetakostyko.test.ui.pages.CatalogPage;
-import by.itacademy.svetakostyko.test.ui.pages.LoginPage;
 import by.itacademy.svetakostyko.test.ui.steps.CatalogStep;
 import by.itacademy.svetakostyko.test.ui.steps.LoginStep;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 
-
 import static by.itacademy.svetakostyko.test.ui.pages.UserData.*;
 
 public class BelbazarTest {
-    WebDriver driver = DriverConfiguration.getDriver();
+    WebDriver driver;
 
     @BeforeEach
     public void setUpBrowser() {
+        driver = DriverConfiguration.getDriver();
         driver.navigate().to(BelbazarPage.URL);
     }
 
     @Test
     @DisplayName("Авторизация с корректными данными")
     public void testLoginUserWithValidData() {
-        LoginStep.stepLoginUser(EMAIL, PASSWORD);
-        Assertions.assertEquals(USER_NAME, LoginPage.getUserName());
+        LoginStep loginStep = new LoginStep();
+        loginStep.stepLoginUser(EMAIL, PASSWORD);
+        Assertions.assertEquals(USER_NAME, loginStep.getUserName());
     }
 
     @Test
     @DisplayName("Авторизация без пароля")
     public void testLoginUserWithEmailOnly() {
-        LoginStep.stepLoginUserWithEmailOnly(EMAIL);
-        Assertions.assertTrue(LoginPage.isErrorMessageValid());
+        LoginStep loginStep = new LoginStep();
+        loginStep.stepLoginUserWithEmailOnly(EMAIL);
+        Assertions.assertTrue(loginStep.isErrorMessageValid());
     }
 
     @Test
     @DisplayName("Авторизация без e-mail")
     public void testLogInWithoutEmail() {
-        LoginStep.stepLoginUserWithPasswordOnly(PASSWORD);
-        Assertions.assertTrue(LoginPage.isErrorMessageValid());
+        LoginStep loginStep = new LoginStep();
+        loginStep.stepLoginUserWithPasswordOnly(PASSWORD);
+        Assertions.assertTrue(loginStep.isErrorMessageValid());
     }
 
     @Test
     @DisplayName("Авторизация без e-mail и без пароля")
     public void testLogInWithoutData() {
-        LoginStep.stepLoginWithoutData();
-        Assertions.assertTrue(LoginPage.isErrorMessageValid());
+        LoginStep loginStep = new LoginStep();
+        loginStep.stepLoginWithoutData();
+        Assertions.assertTrue(loginStep.isErrorMessageValid());
     }
 
     @Test
     @DisplayName("Добавление товара в корзину")
     public void testBasketItem() {
-        Product expectedProduct = CatalogStep.putProductIntoBasket(CatalogPage.TEXT_OF_SEARCHING);
-        Product actualProduct = CatalogStep.getProductFromBasket();
+        CatalogStep catalogStep = new CatalogStep();
+        Product expectedProduct = catalogStep.putProductIntoBasket(CatalogPage.TEXT_OF_SEARCHING);
+        Product actualProduct = catalogStep.getProductFromBasket();
         Assertions.assertEquals(expectedProduct.toString(), actualProduct.toString());
     }
 
     @Test
     @DisplayName("Выход из учетной записи")
     public void testLogOut() {
-        LoginStep.stepLogout(EMAIL, PASSWORD);
-        Assertions.assertTrue(LoginPage.isUserEmpty());
+        LoginStep loginStep = new LoginStep();
+        loginStep.stepLoginUser(EMAIL, PASSWORD);
+        loginStep.stepLogout();
+        Assertions.assertTrue(loginStep.isUserEmpty());
     }
 
     @AfterEach
     void closeDriver() {
-        driver.quit();
+        DriverConfiguration.closeDriver();
     }
 }
