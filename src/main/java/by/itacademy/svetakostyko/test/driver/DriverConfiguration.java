a302472b6fb5f5b730fec7f411c4234dfcba5ba3
Sveta <skostyko@gmail.com>
2023-04-02 14:29:36 +0200

    refactor DriverConfiguration

--- src/main/java/by/itacademy/svetakostyko/test/driver/DriverConfiguration.java
+++ src/main/java/by/itacademy/svetakostyko/test/driver/DriverConfiguration.java
@@ -5,18 +5,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverConfiguration {
-    private static WebDriver driver;
-
-    static {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*");
-        chromeOptions.addArguments("--disable-notifications");
-        chromeOptions.addArguments("start-maximized");
-        driver = new ChromeDriver(chromeOptions);
-//        driver.navigate().to(BelbazarPage.URL);
-    }
+    private static ThreadLocal<WebDriver> driver=new ThreadLocal<>();
 
     public static WebDriver getDriver() {
-        return driver;
+        if(driver.get() == null){
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--remote-allow-origins=*");
+            chromeOptions.addArguments("--disable-notifications");
+            chromeOptions.addArguments("start-maximized");
+            WebDriver webDriver = new ChromeDriver(chromeOptions);
+            driver.set(webDriver);
+        }
+        return driver.get();
+    }
+
+    public static void closeDriver() {
+        driver.get().quit();
+        driver.set(null);
     }
 }
