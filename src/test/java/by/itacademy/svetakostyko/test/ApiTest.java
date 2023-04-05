ad0f8d3de78aed99376e89491e59bfe636b33d8f
Sveta <skostyko@gmail.com>
2023-04-05 13:10:07 +0200

    refactor ApiConfiguration

--- src/test/java/by/itacademy/svetakostyko/test/ApiTest.java
+++ src/test/java/by/itacademy/svetakostyko/test/ApiTest.java
@@ -1,5 +1,6 @@
 package by.itacademy.svetakostyko.test;
 
+import by.itacademy.svetakostyko.test.api.ApiConfiguration;
 import by.itacademy.svetakostyko.test.ui.pages.BelbazarPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -13,9 +14,8 @@ public class ApiTest {
     @Test
     @DisplayName("Получение ответа 200 от запроса Get")
     public void testGetRequest() {
-        String URL = "https://belbazar24.by/";
         when()
-                .get(BelbazarPage.URL)
+                .get(ApiConfiguration.ENDPOINT_URL)
                 .then()
                 .statusCode(200);
     }
@@ -27,19 +27,20 @@ public class ApiTest {
                 .multiPart("sendform", "auth")
                 .multiPart("info[login]", "123tfh123@mail.ru")
                 .multiPart("info[pass]", "bh123bh")
-                .get("https://belbazar24.by/")
+                .get(ApiConfiguration.ENDPOINT_URL)
                 .then()
                 .statusCode(200);
     }
+
     @Test
     @DisplayName("Поиск товара")
     public void testSearch() {
         String response = given()
-                .param("search", "%D0%9A%D0%BE%D1%81%D1%82%D1%8E%D0%BC%D1%8B")
-                .get("https://belbazar24.by/women/")
+                .param("search", ApiConfiguration.CODE_OF_SEARCH)
+                .get(ApiConfiguration.ENDPOINT_URL + "women/")
                 .then()
                 .statusCode(200)
                 .extract().asString();
-                Assertions.assertTrue(response.contains("Костюмы"));
+        Assertions.assertTrue(response.contains(ApiConfiguration.EXPECTED_TEXT));
     }
 }
