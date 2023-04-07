1df0cd17f18e27a1ef4d6980277dff7ebb329e60
Sveta <skostyko@gmail.com>
2023-04-07 17:55:33 +0200

    refactor ApiTest

--- src/test/java/by/itacademy/svetakostyko/test/ApiTest.java
+++ src/test/java/by/itacademy/svetakostyko/test/ApiTest.java
@@ -40,6 +40,6 @@ public class ApiTest {
                 .then()
                 .statusCode(200)
                 .extract().asString();
-        Assertions.assertTrue(response.contains(ApiConfiguration.EXPECTED_TEXT));
+        Assertions.assertTrue(response.contains("Костюмы"));
     }
 }
