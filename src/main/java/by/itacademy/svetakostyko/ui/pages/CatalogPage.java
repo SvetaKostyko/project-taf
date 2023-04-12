3477d04a738226db6dfa91b0e39a146b96bb03fb
Sveta <skostyko@gmail.com>
2023-04-12 11:05:25 +0200

    refactor CatalogPage

--- src/main/java/by/itacademy/svetakostyko/ui/pages/CatalogPage.java
+++ src/main/java/by/itacademy/svetakostyko/ui/pages/CatalogPage.java
@@ -8,16 +8,17 @@ import org.openqa.selenium.WebDriver;
 public class CatalogPage {
 
     private final static String SEARCH_FIELD = "//input[@placeholder='Поиск товара']";
-    private final static String SEARCH_BUTTON = "//div[@class='top_search_button']";
+    private final static String SEARCH_BUTTON = "top_search_button";
     private final static String BRAND_NAME = "(//div[@class='product_item_dop'])[1]";
     private final static String CODE_OF_PRODUCT = "(//div[@class='product_item_art'])[1]";
     private final static String BASKET_BUTTON = "(//div[@class='product_item_basket but'])[1]";
     private final static String SIZE_BUTTON = "(//div[@class='prod_size_item'])[1]";
     private final static String BUTTON_TO_BASKET = "//div[@class='button blue to_basket']";
-    private final static String TOP_BASKET = "//a[@class='top_block_link basket']";
+
+   private final static String TOP_BASKET = "//a[@class='top_block_link basket']";
     private final static String BRAND_IN_BASKET = "//div[@class='basket_item_brand']";
     private final static String CODE_IN_BASKET = "(//div[@class='param']//div[@class='rightText'])[2]";
-    private final static String SIZE_OF_PRODUCT_IN_BASKET = "//div[@class='basket_size_box']";
+    private final static String SIZE_OF_PRODUCT_IN_BASKET = "basket_size_box";
     private final static String PRICE_OF_PRODUCT = "(//div[@class='product_item_i']//span[@class='price'])[1]";
     private final static String PRICE_OF_PRODUCT_IN_BASKET = "//div[@class='oneParam noLine']//span[@class='priceOneProd']";
 
@@ -30,7 +31,7 @@ public class CatalogPage {
 
     public void searchForProducts(String productCategory) {
         driver.findElement(By.xpath(SEARCH_FIELD)).sendKeys(productCategory);
-        driver.findElement(By.xpath(SEARCH_BUTTON)).click();
+        driver.findElement(By.className(SEARCH_BUTTON)).click();
     }
 
     public Product putFirstProductIntoBasket() {
@@ -53,7 +54,7 @@ public class CatalogPage {
         product.brand = Util
                 .waitForElementToBeVisibleByXPath(driver, BRAND_IN_BASKET, 3).getText();
         product.code = driver.findElement(By.xpath(CODE_IN_BASKET)).getText();
-        product.size = driver.findElement(By.xpath(SIZE_OF_PRODUCT_IN_BASKET)).getText();
+        product.size = driver.findElement(By.className(SIZE_OF_PRODUCT_IN_BASKET)).getText();
         product.price = driver.findElement(By.xpath(PRICE_OF_PRODUCT_IN_BASKET)).getText();
         return product;
     }
